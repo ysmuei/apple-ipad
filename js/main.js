5d397acfb1d792e17968410f759c286c7489e808
ysmuei <ysmuei@naver.com>
2024-01-19 20:45:17 +0900

    검색바 추가

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
-const basketStarterEl = document.querySelector("header .basket-starter");
-const basketEl = basketStarterEl.querySelector(".basket");
+// 장바구니!
+const basketStarterEl = document.querySelector('header .basket-starter');
+const basketEl = basketStarterEl.querySelector('.basket');
 
-basketStarterEl.addEventListener("click", function (event) {
+basketStarterEl.addEventListener('click', function (event) {
   event.stopPropagation();
   if (basketEl.classList.contains("show")) {
     hideBasket()
@@ -9,16 +10,36 @@ basketStarterEl.addEventListener("click", function (event) {
     showBasket()
   }
 });
-basketEl.addEventListener("click", function (event) {
+basketEl.addEventListener('click', function (event) {
   event.stopPropagation()
 });
-window.addEventListener("click", function () {
+window.addEventListener('click', function () {
   hideBasket()
 });
 
 function showBasket() {
-  basketEl.classList.add("show");
+  basketEl.classList.add('show');
 }
 function hideBasket() {
-  basketEl.classList.remove("show");
+  basketEl.classList.remove('show');
+}
+
+// 검색!
+const headerEl = document.querySelector('header')
+const searchWrapEl = headerEl.querySelector('.search-wrap')
+const searchStarterEl = headerEl.querySelector('.search-starter')
+const searchCloserEl = searchWrapEl.querySelector('.search-closer')
+const searchShadowEl = searchWrapEl.querySelector('.shadow')
+
+searchStarterEl.addEventListener('click', showSearch)
+searchCloserEl.addEventListener('click', hideSearch)
+searchShadowEl.addEventListener('click', hideSearch)
+
+function showSearch() {
+  headerEl.classList.add('searching')
+  document.documentElement.classList.add('fixed')
+}
+function hideSearch() {
+  headerEl.classList.remove('searching')
+  document.documentElement.classList.remove('fixed')
 }
