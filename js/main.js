eda211b6d7d40394c4cce97ae67e1bf55aa4d18e
ysmuei <ysmuei@naver.com>
2024-02-12 23:34:19 +0900

    마지막 반응형 추가

--- js/main.js
+++ js/main.js
@@ -222,3 +222,11 @@ navigations.forEach(nav => {
 
 const thisYearEl = document.querySelector('span.this-year')
 thisYearEl.textContent = new Date().getFullYear()
+
+const mapEls = document.querySelectorAll('footer .navigations .map')
+mapEls.forEach(function (el) {
+  const h3El = el.querySelector('h3')
+  h3El.addEventListener('click', function () {
+    el.classList.toggle('active')
+  })
+})
